79d747f657cee0d6e186e582eb93d4aba15edbe4
Marcos <vitorcorddesouza@gmail.com>
2024-04-03 17:54:24 -0300

    feat: criação da função de rolagem e adição da cor cinza ao botão

---
+++ ProvaMAUI1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProvaMAUI1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ProvaMAUI1/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+namespace ProvaMAUI1
+{
+    public partial class MainPage : ContentPage
+    {
+        int quantidadeLados = 0;
+        int numeroSorteado = 0;
+        Random rnd = new Random();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDiceRollClicked(object sender, EventArgs e)
+        {
+            if (DiceSidesPck.SelectedIndex != -1) {
+                quantidadeLados = (int)DiceSidesPck.ItemsSource[DiceSidesPck.SelectedIndex];
+                numeroSorteado = rnd.Next(1, quantidadeLados+1);
+                DiceNumberLbl.Text = $"{numeroSorteado}";
+
+            }
+        } 
+    }
+}
